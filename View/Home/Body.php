4e9f82fb14efdd80329614537ad4c74a9bfc88a6
evandrorisso <evandrohrisso@gmail.com>
2017-04-17 13:45:51 -0300

    Atualizando

--- View/Home/Body.php
+++ View/Home/Body.php
@@ -5,7 +5,12 @@
 	?>
 	
 		<div class="row" align="center">
-			<h1>Controle de Quentinhas</h1>
+			<div class="panel panel-primary">
+  				<div class="panel-heading">Controle de Quentinhas</div>
+  				<div class="panel-body">
+    				Panel content
+  				</div>
+			</div>
 		</div>
 		<div class="copyright">
 		<div class="row">
