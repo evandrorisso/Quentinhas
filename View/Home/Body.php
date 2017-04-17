179d784dff064ed210668d457537666f3eda6eb8
evandrorisso <evandrohrisso@gmail.com>
2017-04-17 14:00:03 -0300

    Inserindo o Jumbotron e link de acesso ao Novo

--- View/Home/Body.php
+++ View/Home/Body.php
@@ -5,13 +5,18 @@
 	?>
 	
 		<div class="row" align="center">
-			<div class="panel panel-primary">
-  				<div class="panel-heading">Controle de Quentinhas</div>
+			<!--<div class="panel panel-primary">
+  				<div class="panel-heading">Sistema de Quentinhas</div>
   				<div class="panel-body">
-    				Panel content
-  				</div>
+    		-->		<div class="jumbotron">
+        				<h1>Controle de Quentinhas</h1>
+        				<p class="lead">Controle de suas vendas de quentinhas de forma simples e pratica.</p>
+        				<p><a class="btn btn-lg btn-primary" href="http://quentinha.exodoti.xyz/Usuario/novo" role="button">Cadastre-se Agora</a></p>
+        				<p><a class="btn btn-lg btn-success" href="#" role="button">Acesse Agora</a></p>
+      				</div>
+  			<!--	</div>
 			</div>
-		</div>
+		--></div>
 		<div class="copyright">
 		<div class="row">
 	 	 	<div class="rodape">
