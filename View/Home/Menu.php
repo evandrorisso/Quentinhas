dbc9a148a018b24f1f43c1f2781d7b6ccae7689d
evandrorisso <evandrohrisso@gmail.com>
2017-04-17 21:13:21 -0300

    Ajustando o menu

--- View/Home/Menu.php
+++ View/Home/Menu.php
@@ -22,7 +22,7 @@
         <li class="active"><a href="http://quentinha.exodoti.xyz/">Aqui <span class="sr-only">(current)</span></a></li>
         <li><a href="#">Link</a></li>
         <li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Cadastro<span class="caret"></span></a>
           <ul class="dropdown-menu">
             <li><a href="http://quentinha.exodoti.xyz/Home/SobreNos">Sobre Nós</a></li>
             <li><a href="#">Another action</a></li>
@@ -34,12 +34,6 @@
           </ul>
         </li>
       </ul>
-      <form class="navbar-form navbar-left">
-        <div class="form-group">
-          <input type="text" class="form-control" placeholder="Search">
-        </div>
-        <button type="submit" class="btn btn-default">Submit</button>
-      </form>
       <ul class="nav navbar-nav navbar-right">
         <li><a href="#">Link</a></li>
         <li class="dropdown">
